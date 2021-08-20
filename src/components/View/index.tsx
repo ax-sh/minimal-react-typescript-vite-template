dd8c9431fbac4abaee78d364117eacaa8a5f5da2
Axm <>
2021-08-20 16:14:43 +0545

    init

---
+++ src/components/View/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const ViewStyled = styled.section``;
+const View = () => {
+  return <ViewStyled>View</ViewStyled>;
+};
+
+export default View;
