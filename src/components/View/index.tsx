4f4f4801c6ad40f2790dad6883805fe681637a09
Axm <>
2021-08-26 12:56:33 +0545

    chore: remove default stories and custom stories

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const ViewStyled = styled.section``;
-const View = () => {
-  return <ViewStyled>View</ViewStyled>;
+const View = (props:any) => {
+  return <ViewStyled {...props}>View</ViewStyled>;
 };
 
 export default View;
